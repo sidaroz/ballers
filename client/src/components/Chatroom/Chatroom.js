1fcdbbd0ce0dc1ee4b5ad8d9b9b5f644dff96759
sidaroz <sidarozbek96@gmail.com>
2022-05-25 20:37:22 +0100

    added chat layout, website looking perfect

--- client/src/components/Chatroom/Chatroom.js
+++ client/src/components/Chatroom/Chatroom.js
@@ -63,7 +63,7 @@ function Chatroom() {
       messageId: nanoid(), //TODO: use any uuid generator (INSTALL NANOID!!!!!! and import)
       username: userData.properUsername,
       message: message,
-      timeStamp: "",
+      timeStamp: `${new Date().getHours()}:${new Date().getMinutes()}`,
       userId: userData.username,
       userImage: userData.image,
     };
@@ -88,7 +88,7 @@ function Chatroom() {
             messageId: incomingData.messageId,
             username: incomingData.username,
             message: incomingData.message,
-            timeStamp: "",
+            timeStamp: incomingData.timeStamp,
             userId: incomingData.userId,
             userImage: incomingData.userImage,
           };
@@ -107,17 +107,40 @@ function Chatroom() {
       <Navbar />
       <div className="chat-box">
         <ul>
-          {listOfMessages.map((item, i) => (
-            <li key={item.messageId}>
-              {`${item.username}: `}
-              {item.message}
-            </li>
-          ))}
+          {listOfMessages.map((item, i) =>
+            item.username === userData.properUsername ? (
+              <li key={item.messageId} className="right-side-chat">
+                <div className="div-holder">
+                  <p className="time-right">{item.timeStamp}</p>
+                  <div className="navbar-profile chat-profile right-side-div">
+                    <p className="username-chat-right">{`${item.username}`}</p>
+                    <img src={item.userImage} alt="user profile"></img>
+                  </div>
+                </div>
+                <p className="chat-para-left">{item.message}</p>
+              </li>
+            ) : (
+              <li key={item.messageId} className="left-side-chat">
+                <div className="div-holder-left">
+                  <div className="navbar-profile chat-profile">
+                    <img src={item.userImage} alt="user profile"></img>
+                    <p className="username-chat">{`${item.username}`}</p>
+                  </div>
+                  <p className="time-left">{item.timeStamp}</p>
+                </div>
+                <p className="chat-para">{item.message}</p>
+              </li>
+            )
+          )}
         </ul>
       </div>
 
       <div className="input-chat">
-        <input className="msg-input" onChange={handleMessageChange} />
+        <input
+          className="msg-input"
+          onChange={handleMessageChange}
+          placeholder="Message..."
+        />
         <button className="send-msg-btn" onClick={handleSendMessage}>
           Send
         </button>
